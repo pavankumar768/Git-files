1488ceb0fba5af86276e885a5b23182766d1a939
pavankumar768 <umarpavan768@gmail.com>
2025-01-08 18:56:26 +0530

    second push

---
+++ day2.py
@@ -0,0 +1,15 @@
+my_list = [10, 20, 30, 40, 50]
+print(my_list[0])   
+print(my_list[2])   
+ 
+my_tuple = (5, 15, 25, 35, 45)
+
+print(my_tuple[1])   
+print(my_tuple[4])   
+ 
+my_dict = {"a": 100, "b": 200, "c": 300, "d": 400, "e": 500}
+
+ 
+print(my_dict["a"])   
+print(my_dict["c"])     
+
