ae661e81ee49d1e49b87c88b560e8a0524d8d3e6
pavankumar768 <umarpavan768@gmail.com>
2025-01-08 18:39:41 +0530

    Ignore 2211cs020214 directory

---
+++ day1.py
@@ -0,0 +1,17 @@
+integer_variable = 10           
+float_variable = 3.14           
+string_variable = "Hello, Jupyter!"   
+boolean_variable = True         
+
+# Print each variable and its type
+print("Integer Variable:")
+print("Value:", integer_variable, "| Type:", type(integer_variable))
+
+print("\nFloat Variable:")
+print("Value:", float_variable, "| Type:", type(float_variable))
+
+print("\nString Variable:")
+print("Value:", string_variable, "| Type:", type(string_variable))
+
+print("\nBoolean Variable:")
+print("Value:", boolean_variable, "| Type:", type(boolean_variable))
